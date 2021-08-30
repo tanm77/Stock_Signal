d0d03cc1278a9ee4700d6abcb8204c037100c87c
tanm77 <47867918+tanm77@users.noreply.github.com>
2021-08-30 20:07:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ import trend
 
 # https://www.nseindia.com/products-services/indices-nifty200-index
 df = pd.read_csv('ind_nifty200list.csv')
+
+ticker_list = pd.Series([x + '.NS' for x in df['Symbol']]).to_string(header=False,index=False).replace(" ", "").replace('\n'," ")
+nse_200_data = yf.download(ticker_list)
+data.to_csv('aggregated_nse_200.csv')
